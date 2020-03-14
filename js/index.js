98d0d8f02a0ab0c1949204dd044358c774367b23
sofi-coder <sofi_kovaluk@ukr.net>
2020-03-14 15:13:33 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const display = document.querySelector('.display');
+
+document.querySelectorAll('.digits button')
+   .forEach( button => button.addEventListener('click', digitPressed));
+   
+function digitPressed(ev) {
+   display.value += ev.target.innerText;
+
+}  
+document.querySelectorAll('.opers button')
+   .forEach( button => button.addEventListener('click', digitPressed));
+   
+function opersPressed(ev) {
+   display.value += ev.target.innerText;
+}  
+document.querySelector('.eq').addEventListener('click', calculate);
+
+function calculate(){
+    display.value = eval(display.value);
+
+}
+calcClear.on('click', function () {
+    calcDisplay.val('');
+});
+function Decimal () 
+{
+		var curReadOut = Fcalc.ReadOut.value;
+		if (FlagNewNum) 
+		{
+			curReadOut = "0.";
+			FlagNewNum = false;
+		}
+		else
+		{
+			if (curReadOut.indexOf(".") == -1)
+				curReadOut += ".";
+		}
+		Fcalc.ReadOut.value = curReadOut;
+}
+
